65248ba805f6a5860a6193703d5f758808e3a065
Danielle S <strickl6@illinois.edu>
2021-10-14 16:20:48 -0500

    added js functionality to mobile carousel

--- script.js
+++ script.js
@@ -3,18 +3,31 @@ const navbar = document.querySelector(".navbar");
 const navitem = document.querySelectorAll(".navitem");
 const close = document.getElementById("close");
 const hamburger = document.getElementById("hamburger");
+const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
   close.classList.toggle("active");
   hamburger.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
 close.addEventListener("click", () => {
   navbar.classList.toggle("active");
   close.classList.toggle("active");
   hamburger.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
+
+// Close navlist when a link is clicked
+
+if (document.documentElement.clientWidth < 1024) {
+  navitem.forEach((link) =>
+    link.addEventListener("click", () => {
+      close.click();
+    })
+  );
+}
 
 // Shopping cart functionality
 const cart = document.querySelector(".nav-cart");
@@ -25,3 +38,48 @@ cart.addEventListener("click", () => {
   shoppingCart.classList.toggle("active");
   noItems.classList.toggle("active");
 });
+
+// Setting up images for carousel
+let i = 0;
+
+images = new Array();
+
+images[0] = new Image();
+images[0].src = "styles/images/image-product-1.jpg";
+images[1] = new Image();
+images[1].src = "styles/images/image-product-2.jpg";
+images[2] = new Image();
+images[2].src = "styles/images/image-product-3.jpg";
+images[3] = new Image();
+images[3].src = "styles/images/image-product-4.jpg";
+
+// Switch images in carousel
+let mainImg = document.getElementById("mainImg");
+let next = document.getElementById("rightArrow");
+let prev = document.getElementById("leftArrow");
+
+mainImg.src = images[i].src;
+
+// rotating through image array to display images
+const nextImage = () => {
+  if (i < images.length - 1) {
+    i++;
+    mainImg.src = images[i].src;
+  } else {
+    i = 0;
+    mainImg.src = images[i].src;
+  }
+};
+
+const prevImage = () => {
+  if (i > 0) {
+    i--;
+    mainImg.src = images[i].src;
+  } else {
+    i = images.length - 1;
+    mainImg.src = images[i].src;
+  }
+};
+
+prev.addEventListener("click", prevImage);
+next.addEventListener("click", nextImage);
